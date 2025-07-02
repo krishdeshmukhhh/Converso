a328499f6f0669f76bd1ee978c39e334c2586baf
Krish Deshmukh <deshmukh.102@osu.edu>
2025-07-02 19:52:00 +0530

    Implement Profile Page, Subscription Checks and Bookmarks

--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -1,5 +1,9 @@
+"use client";
+import { removeBookmark, addBookmark } from "@/lib/actions/companion.actions";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 interface CompanionCardProps {
     id: string;
@@ -8,16 +12,45 @@ interface CompanionCardProps {
     subject: string;
     duration: number;
     color: string;
+    bookmarked: boolean;
 }
 
-const CompanionCard = ({ id, name, topic, subject, duration, color }: CompanionCardProps) => {
+const CompanionCard = ({
+                           id,
+                           name,
+                           topic,
+                           subject,
+                           duration,
+                           color,
+                           bookmarked,
+                       }: CompanionCardProps) => {
+    const pathname = usePathname();
+    const [isBookmarked, setIsBookmarked] = useState(bookmarked);
+
+    const handleBookmark = async () => {
+        if (isBookmarked) {
+            await removeBookmark(id, pathname);
+            setIsBookmarked(false);
+        } else {
+            await addBookmark(id, pathname);
+            setIsBookmarked(true);
+        }
+    };
+
     return (
         <article className="companion-card" style={{ backgroundColor: color }}>
             <div className="flex justify-between items-center">
                 <div className="subject-badge">{subject}</div>
-                <button className="companion-bookmark">
-                    <Image src="/icons/bookmark.svg" alt="bookmark"
-                        width={12.5} height={15}
+                <button className="companion-bookmark" onClick={handleBookmark}>
+                    <Image
+                        src={
+                            isBookmarked
+                                ? "/icons/bookmark-filled.svg"
+                                : "/icons/bookmark.svg"
+                        }
+                        alt="bookmark"
+                        width={12.5}
+                        height={15}
                     />
                 </button>
             </div>
@@ -25,7 +58,12 @@ const CompanionCard = ({ id, name, topic, subject, duration, color }: CompanionC
             <h2 className="text-2xl font-bold">{name}</h2>
             <p className="text-sm">{topic}</p>
             <div className="flex items-center gap-2">
-                <Image src="/icons/clock.svg" alt="duration" width={13.5} height={13.5} />
+                <Image
+                    src="/icons/clock.svg"
+                    alt="duration"
+                    width={13.5}
+                    height={13.5}
+                />
                 <p className="text-sm">{duration} minutes</p>
             </div>
 
@@ -35,6 +73,7 @@ const CompanionCard = ({ id, name, topic, subject, duration, color }: CompanionC
                 </button>
             </Link>
         </article>
-    )
-}
-export default CompanionCard
+    );
+};
+
+export default CompanionCard;
